3b336ec8a67bc9316b4b43685e75698cfda083fa
NantoYuma <naitoyuma@outlook.jp>
2021-12-16 23:33:01 +0900

    first

---
+++ js/notice_add.js
@@ -0,0 +1,127 @@
+(function(){
+  let notice_add = new Vue({
+    el :"#notice_add",
+    data: {
+      message:"お知らせを追加",
+      articles:[],
+      newItem:{
+        title:"ここにタイトルを入力",
+        comment:"ここにコメントを入力",
+        day:"1991-11-18",
+        checkAll: false,
+        deleteId:false,
+        target:[{
+          id:1,
+          name:"訪問看護",
+          check: false,
+        },{
+          id:2,
+          name:"訪問介護",
+          check: false,
+        },{
+          id:3,
+          name:"通所介護",
+          check: false,
+        },{
+          id:4,
+          name:"訪問リハビリ",
+          check: false,
+        },{
+          id:5,
+          name:"入所介護",
+          check: false,
+        },{
+          id:6,
+          name:"入所リハビリ",
+          check: false,
+        }]
+      },
+
+    },
+    mounted:function(){
+      this.articles = JSON.parse(localStorage.getItem("articles")) || [];
+    },
+    watch:{
+      articles:{
+        handler: function(){
+          localStorage.setItem("articles",JSON.stringify(this.articles));
+        },
+        deep: true
+      }
+    },
+    methods: {
+      addItem: function(){
+        let item = {
+          title: this.newItem.title,
+          comment: this.newItem.comment,
+          day: this.newItem.day,
+          checkAll: this.newItem.checkAll,
+          deleteId:this.newItem.deleteId,
+          target: this.newItem.target
+        };
+        this.articles.push(item);
+
+        // localStorage.setItem("articles", JSON.stringify(this.articles));
+        // localStorage.setItem("test", JSON.stringify(this.testdata));
+        alert("お知らせを追加しました");
+        this.newItem = [{
+          title:"ここにタイトルを入力",
+          comment:"ここにコメントを入力",
+          day:"1991-11-18",
+          checkAll: false ,
+          target:[{
+            id:1,
+            name:"訪問看護",
+            check: false,
+          },{
+            id:2,
+            name:"訪問介護",
+            check: false,
+          },{
+            id:3,
+            name:"通所介護",
+            check: false,
+          },{
+            id:4,
+            name:"訪問リハビリ",
+            check: false,
+          },{
+            id:5,
+            name:"入所介護",
+            check: false,
+          },{
+            id:6,
+            name:"入所リハビリ",
+            check: false,
+          }]
+        }]
+      },
+      checkAll: function(){
+        if(this.newItem.checkAll){ 
+          for(let i = 0; i < this.newItem.target.length; i++){
+            this.newItem.target[i].check = true;
+          }
+        }else{
+          for(let i = 0; i < this.newItem.target.length; i++){
+            this.newItem.target[i].check = false;
+          }
+        }
+      },
+      checkChild: function(){
+        let count = 0;
+        for(let i = 0; i < this.newItem.target.length; i++){
+          if(this.newItem.target[i].check){
+            count ++;
+          }
+        }
+        if(count >= this.newItem.target.length){
+          this.newItem.checkAll = true;
+        }else{
+          console.log(count);
+          this.newItem.checkAll = false;
+        }
+      }
+    }
+
+  })
+})();
---
+++ js/notiec.js
@@ -0,0 +1,83 @@
+(function(){
+  'use strict';
+
+  let notice = new Vue({
+    el :"#notice_vue",
+    data:{
+      articles:[]
+    },
+    watch:{
+      articles:{
+        handler: function(){
+          localStorage.setItem("articles",JSON.stringify(this.articles));
+        },
+        deep: true
+      }
+    },
+    methods: {
+      deleteItem: function(index){
+        if(confirm("この記事を削除してもいいですか？")){
+          this.articles.splice(index, 1);
+        }
+      },
+      deleteTag: function(i, index){
+        this.articles[index].target[i].check = false;
+        
+        
+      },
+      checkAll: function(index){
+        if(this.articles[index].checkAll){ 
+          for(let i = 0; i < this.articles[index].target.length; i++){
+            this.articles[index].target[i].check = true;
+          }
+        }else{
+          for(let i = 0; i < this.articles[index].target.length; i++){
+            this.articles[index].target[i].check = false;
+          }
+        }
+      },
+      checkChild: function(index){
+        let count = 0;
+        for(let i = 0; i < this.articles[index].target.length; i++){
+          if(this.articles[index].target[i].check){
+            count ++;
+          }
+        }
+        if(count >= this.articles[index].target.length){
+          this.articles[index].checkAll = true;
+        }else{
+          console.log(count);
+          this.articles[index].checkAll = false;
+        }
+      },
+      checkedDelete: function(){
+        confirm("チェックした記事を全て削除します");
+        this.articles = this.deletedArray;
+      }
+    },
+    mounted:function(){
+      this.articles = JSON.parse(localStorage.getItem("articles")) || [];
+    },
+    computed:{
+      deleteIdCount: function(){
+        let count = 0;
+        for(let i = 0; i < this.articles.length; i++){
+          if(this.articles[i].deleteId){
+            count ++;
+          }
+          if(count > 0){
+            return true;
+          }
+        }
+      },
+      deletedArray: function(){
+        let deletedItem = this.articles.filter(function(d){
+          return !d.deleteId;
+        });
+        return deletedItem;
+      }
+    }
+  })
+
+
+})();
